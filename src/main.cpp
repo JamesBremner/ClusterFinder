c5961dc6112cfd9de6db2897e4232e4968908db5
JamesBremner <james@ravenspoint.com>
2023-12-03 12:39:38 -0500

    Quadtree search

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,12 @@
 
 #include "cxy.h"
 #include "KMeans.h"
+#include "quadtree.h"
 #include <wex.h>
 #include "cRunWatch.h"
 
 std::vector<cxy> vTarget;
+quad::cCell *theQuadtree;
 double theDim;
 int radius;
 int theMax2;
@@ -22,7 +24,7 @@ void generate(double dim, double count)
 {
     theDim = dim;
 
-    srand (time(NULL));
+    srand(time(NULL));
 
     std::vector<cxy> vCenter;
     for (int ck = 0; ck < 5; ck++)
@@ -40,6 +42,14 @@ void generate(double dim, double count)
         vTarget.emplace_back(x, y);
     }
     std::cout << "generated " << vTarget.size() << " targets\n";
+}
+
+void buildQuadtree()
+{
+    raven::set::cRunWatch aWatcher("buildQuadtree");
+    theQuadtree = new quad::cCell(quad::cPoint(theDim / 2, theDim / 2), theDim);
+    for (cxy &t : vTarget)
+        theQuadtree->insert(quad::cPoint(t.x, t.y));
 }
 
 int countNeighbors(const cxy &aim)
@@ -62,7 +72,29 @@ void gridSearch(double radius)
         for (double x = 1; x < theDim; x++)
         {
             cxy aim((int)x, (int)y);
-            int count = countNeighbors( aim );
+            int count = countNeighbors(aim);
+            if (count > bestCount)
+            {
+                bestCount = count;
+                bestAim = aim;
+            }
+        }
+}
+
+void quadSearch(double radius)
+{
+    raven::set::cRunWatch aWatcher("quadSearch");
+
+    bestCount = 0;
+    for (double y = 1; y < theDim; y++)
+        for (double x = 1; x < theDim; x++)
+        {
+            cxy aim((int)x, (int)y);
+
+            int count = theQuadtree->find(
+                                       quad::cCell(quad::cPoint(aim.x, aim.y), 2 * radius))
+                            .size();
+
             if (count > bestCount)
             {
                 bestCount = count;
@@ -104,8 +136,8 @@ void findClustersKMeans(double radius)
         cxy aim;
         aim.x = c.center().d[0];
         aim.y = c.center().d[1];
-        int count = countNeighbors( aim );
-        if( count > bestCount )
+        int count = countNeighbors(aim);
+        if (count > bestCount)
         {
             bestCount = count;
             bestAim = aim;
@@ -116,54 +148,154 @@ void findClustersKMeans(double radius)
 class cGUI
 {
 public:
-    cGUI()
-        : fm(wex::maker::make())
-    {
+    cGUI();
 
-        raven::set::cRunWatch::Start();
-        radius = 40;
-        generate(500, 500);
+    void draw(wex::shapes &S);
+
+private:
+    wex::gui &fm;
+
+    int Gridhits;
+    int QuadHits;
+    int KmeansHits;
+    cxy GridAim;
+    cxy QuadAim;
+    cxy KmeansAim;
+
+    enum class eAlgo
+    {
+        grid,
+        quad,
+        kmeans,
+    };
+
+    eAlgo myAlgo;
+
+    void search(eAlgo a);
+
+    void constructMenu();
+};
+
+cGUI::cGUI()
+    : fm(wex::maker::make()),
+      myAlgo(eAlgo::grid)
+{
+
+    raven::set::cRunWatch::Start();
+    radius = 40;
+    generate(500, 500);
+    search(eAlgo::grid);
+    search(eAlgo::kmeans);
+    search(eAlgo::quad);
+
+    raven::set::cRunWatch::Report();
+
+    fm.move(50, 50, 700, 700);
+    fm.text("ClusterFinder");
+
+    constructMenu();
+
+    fm.events().draw(
+        [&](PAINTSTRUCT &ps)
+        {
+            wex::shapes S(ps);
+            draw(S);
+        });
+
+    fm.show();
+    fm.run();
+}
+
+void cGUI::constructMenu()
+{
+    wex::menubar mbar(fm);
+
+    wex::menu view(fm);
+    view.append(
+        "Grid",
+        [&](const std::string &title)
+        {
+            myAlgo = eAlgo::grid;
+            fm.text("ClusterFinder Grid search");
+            fm.update();
+        });
+    view.append(
+        "Quad",
+        [this](const std::string &title)
+        {
+            myAlgo = eAlgo::quad;
+            fm.text("ClusterFinder Quadtree search");
+            fm.update();
+        });
+    view.append(
+        "Kmeans",
+        [this](const std::string &title)
+        {
+            myAlgo = eAlgo::kmeans;
+            fm.text("ClusterFinder Kmeans search");
+            fm.update();
+        });
+    mbar.append("View", view);
+}
+void cGUI::draw(wex::shapes &S)
+{
+    switch (myAlgo)
+    {
+    case eAlgo::grid:
+        bestAim = GridAim;
+        break;
+
+    case eAlgo::quad:
+        bestAim = QuadAim;
+        break;
+
+    case eAlgo::kmeans:
+        bestAim = KmeansAim;
+        break;
+    }
+    int scale = 1;
+    S.color(0x0000FF);
+    S.fill(false);
+    S.circle((int)bestAim.x * scale, (int)bestAim.y * scale, radius);
+
+    S.color(0xFF0000);
+    // S.fill();
+    for (cxy e : vTarget)
+    {
+        e.x *= scale;
+        e.y *= scale;
+        S.rectangle({(int)e.x - 2, (int)e.y - 2,
+                     5, 5});
+    }
+}
+
+void cGUI::search(eAlgo a)
+{
+    switch (a)
+    {
+    case eAlgo::grid:
         gridSearch(radius);
         std::cout << "Best count, grid " << bestCount << "\n";
+        Gridhits = bestCount;
+        GridAim = bestAim;
+        break;
+
+    case eAlgo::quad:
+        buildQuadtree();
+        quadSearch(radius);
+        std::cout << "Best count, quadtree " << bestCount << "\n";
+        QuadHits = bestCount;
+        QuadAim = bestAim;
+        break;
+
+    case eAlgo::kmeans:
         findClustersKMeans(radius);
         std::cout << "Best count, KMeans " << bestCount << "\n";
-        raven::set::cRunWatch::Report();
-
-        fm.move(50, 50, 700, 700);
-        fm.text("ClusterFinder");
-
-        fm.events().draw(
-            [&](PAINTSTRUCT &ps)
-            {
-                wex::shapes S(ps);
-                draw(S);
-            });
-
-        fm.show();
-        fm.run();
+        KmeansHits = bestCount;
+        KmeansAim = bestAim;
+        break;
     }
-
-    virtual void draw(wex::shapes &S)
-    {
-        int scale = 1;
-        S.color(0x0000FF);
-        S.fill(false);
-        S.circle((int)bestAim.x * scale, (int)bestAim.y * scale, radius);
-
-        S.color(0xFF0000);
-        // S.fill();
-        for (cxy e : vTarget)
-        {
-            e.x *= scale;
-            e.y *= scale;
-            S.rectangle({(int)e.x - 2, (int)e.y - 2,
-                         5, 5});
-        }
-    }
-
-protected:
-    wex::gui &fm;
-};
+}
 
 main()
 {
